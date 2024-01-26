12ffe2853bdfc92a912c59c5b1b1fc1f2f004049
04kelfos <kelseybetty@hotmail.com>
2024-01-26 18:20:12 +0900

    added basic Search function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Dictionary App</h1>
+      <div className="container">
+        <header className="App-header"></header>
+        <h1>Dictionary App</h1>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded by Kelsey Foster open-sourced on{" "}
+          <a
+            href="https://github.com/04kelfos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          and hosted on Netlify
+        </footer>
+      </div>
     </div>
   );
 }
