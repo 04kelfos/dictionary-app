6b50b8aa6d95828e9795531823d22e1d7e7386c5
04kelfos <kelseybetty@hotmail.com>
2024-01-30 20:48:47 +0900

    added the response call for definition and example

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results.js";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data);
+    setResults(response.data);
   }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
     let apiKey = "107f301285cb6tcc0f0b30ab311aao4a";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -24,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
