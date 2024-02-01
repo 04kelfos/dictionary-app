0be39db561141734c442a94f0905c1bc35e3cc58
04kelfos <kelseybetty@hotmail.com>
2024-02-01 20:47:29 +0900

    added phonetics

--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,10 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2 className="text-capitalize">{props.results.word}</h2>
+        <div>
+          <strong>Phonetic:</strong> {""}
+          {props.results.phonetic}
+        </div>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
